b50c3f08929efd5a26a0b4b27a2c78499b8df8ae
ski14hs <sean.k.ishikawa@gmail.com>
2019-03-28 20:10:13 -0700

    added css and temp calc

--- app.js
+++ app.js
@@ -32,16 +32,18 @@ function getLatLong(){
             var weatherData = JSON.parse(this.response);
             // {"coord":{"lon":-118.13,"lat":34.09},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":289.3,"pressure":1021,"humidity":39,"temp_min":287.04,"temp_max":291.15},"visibility":16093,"wind":{"speed":3.1,"deg":150},"clouds":{"all":1},"dt":1553825456,"sys":{"type":1,"id":3514,"message":0.009,"country":"US","sunrise":1553780719,"sunset":1553825379},"id":5323060,"name":"Alhambra","cod":200}
             var city = weatherData.name;
-            var temp = weatherData.main.temp;
+            var temp = weatherData.main.temp ;
             var description = weatherData.weather[0].main;
             var icon = weatherData.weather[0].icon;
             console.log(city + "|" + temp + "|" + description + "|" + icon);
+
+            temp = (((temp - 273.15) * 9/5) + 32).toFixed(1);
 
             
             var cityHeader = document.createElement("h2");
             cityHeader.textContent = city;
             var tempHeader = document.createElement("h2");
-            tempHeader.textContent= temp;
+            tempHeader.textContent= temp + ' °F';
             var descHeader = document.createElement("h2");
             descHeader.textContent = description;
             var image = document.createElement("img");
